90588dc5a64171174e71430afeebbd794bdab530
JoelCAI <102713088+JoelCAI@users.noreply.github.com>
2026-05-14 21:28:03 -0300

    feat: update search

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
     <div className={styles.searchContainer}>
       <input 
         type="text" 
-        placeholder="Buscar muebles, lámparas, mesas..." 
+        placeholder="Buscar..." 
         className={styles.searchInput}
       />
       <button className={styles.searchButton}>
